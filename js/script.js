961c6a594eaee6aadb3abb7e938dd00613daa271
GregoryVDev <arexo@hotmail.fr>
2024-04-15 13:32:01 +0200

    Added function guessField and turn

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Générer un nombre aléatoire entre 1 et 100.
-// Stocker le nombre de tours déjà joués. Commencer par 1.
-// Fournir au joueur le moyen de saisir un nombre.
-// Stocker l'ensemble des propositions de nombres pour que le joueur puisse les consulter.
+// Générer un nombre aléatoire entre 1 et 100. (ok)
+// Stocker le nombre de tours déjà joués. Commencer par 1.(ok)
+// Fournir au joueur le moyen de saisir un nombre.(ok)
+// Stocker l'ensemble des propositions de nombres pour que le joueur puisse les consulter.(ok)
 // Vérifier si le nombre saisi par le joueur est correct.
 // S'il est correct :
 // Afficher un message de félicitations.
@@ -16,3 +16,35 @@
 // Empêcher que le joueur saisisse de nouveau un nombre.
 // Afficher un contrôle pour que le joueur puisse rejouer.
 // Une fois le jeu redémarré, s'assurer que la logique du jeu et l'interface utilisateur sont complètement réinitialisées, puis revenir à l'étape 1.
+
+let minimum = 1; // Lower Number
+let maximum = 100; // High Number
+const numberRandom = Math.floor(Math.random() * (maximum - minimum)) + minimum; // Put the random number
+let numberTurns = 1; // Number of turns
+let numberOfTrials = 10; // Trials number
+let smallNumber = "Votre nombre choisi est trop petit";
+let largeNumber = "Votre nombre choisi est trop grand";
+let oneChance = "Attention !";
+let userGuess = parseInt(guessField).value;
+
+function guessField(numberOfTrials) {
+  let numberChoise = document.getElementById("debug").value;
+  // document.getElementById("guessField").innerHTML = numberChoise;
+  turn(numberChoise);
+}
+
+function turn(numberChoise) {
+  if (numberOfTrials > 0) {
+    numberOfTrials--;
+    if (numberChoise > numberRandom) {
+      console.log(`${largeNumber} il vous reste ${numberOfTrials} essais`);
+    } else if (numberChoise < numberRandom) {
+      console.log(`${smallNumber} il vous reste ${numberOfTrials} essais`);
+    } else {
+      console.log("Bravo mon ami tu as trouvé !");
+    }
+  } else {
+    console.log("Tu as malheureusement perdu... Dommage !");
+    finDuJeu();
+  }
+}
